09b9327cc405a995bbeb4e4bd7529271fb90f04b
Thiago Panchiniak <panchiniak@gmail.com>
2020-04-16 18:44:22 -0300

    add nodemon into dev dependencies, update index.js

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  return res.json({ message: 'Rodando' });
+const users = ['user01', 'user02', 'user03', 'user04']
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
 })
 
 server.listen(3000);
